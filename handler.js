6f88b1e0248854b485360bbda3f0fb4875bbc2a7
Perez1503 <franklinsilvestreperezmendez@gmail.com>
2024-09-25 18:57:42 -0600

    VentasPerzzz

--- handler.js
+++ handler.js
@@ -1084,7 +1084,7 @@ export async function participantsUpdate({ id, participants, action }) {
       if (chat.welcome && !chat?.isBanned) {
         const groupMetadata = await m?.conn?.groupMetadata(id) || (conn?.chats[id] || {}).metadata;
         for (const user of participants) {
-          let pp = 'https://raw.githubusercontent.com/KillzN/VentasPerzzz/master/src/avatar_contact.png';
+          let pp = 'https://raw.githubusercontent.com/Perez1503/VentasPerzzz/master/src/avatar_contact.png';
           try {
             pp = await m?.conn?.profilePictureUrl(user, 'image');
           } catch (e) {
